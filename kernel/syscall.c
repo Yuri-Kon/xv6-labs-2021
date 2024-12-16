82dc3e6310c60474045e7a6c2adf1a179b22df85
Yuri-kon <superyurikon@icloud.com>
2024-12-17 00:08:24 +0800

    finish syscall

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -132,13 +132,20 @@ static uint64 (*syscalls[])(void) = {
 void
 syscall(void)
 {
+  char const *syscall_names[] = {"fork", "exit", "wait", "pipe", "read",
+  "kill", "exec", "fstat", "chdir", "dup", "getpid", "sbrk", "sleep",
+  "uptime", "open", "write", "mknod", "unlink", "link", "mkdir","close","trace","sysinfo"};
   int num;
   struct proc *p = myproc();
 
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
-  } else {
+  }if((p->mask) & (1 << num))
+  {
+    printf("%d: syscall %s -> %d\n", p->pid, syscall_names[num-1], p->trapframe->a0);
+  } 
+  else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
